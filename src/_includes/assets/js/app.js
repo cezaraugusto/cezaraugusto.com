6b07a91ec75ac4a2d87512466ce9f3546db1a307
Cezar Augusto <boss@cezaraugusto.net>
2020-04-07 21:44:06 -0300

    Several interface updates for the post page

--- src/_includes/assets/js/app.js
+++ src/_includes/assets/js/app.js
@@ -3,3 +3,18 @@ window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js', { scope: '/' })
   }
 })
+
+document.querySelector('#copyLink')
+  .addEventListener('click', (event) => {
+    event.preventDefault()
+    writeToClipboard(event.target.href)
+    document.querySelector('.clipboard').classList.toggle('fadeInOut')
+  })
+
+function writeToClipboard (text) {
+  navigator.clipboard.writeText(text)
+    .then(
+      () => console.log('Copied to clipboard!'),
+      reason => console.error('Could not copy to clipboard:', reason)
+    )
+}
