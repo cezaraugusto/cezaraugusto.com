22e975c0c6ccefa36869ba5efa1e7065acf04540
Cezar Augusto <boss@cezaraugusto.net>
2020-08-19 00:59:07 -0300

    New interface

--- src/_includes/assets/js/app.js
+++ src/_includes/assets/js/app.js
@@ -1,15 +1,22 @@
+
 window.addEventListener('load', () => {
   if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js', { scope: '/' })
   }
 })
 
-document.querySelector('#copyLink')
-  .addEventListener('click', (event) => {
-    event.preventDefault()
-    writeToClipboard(event.target.href)
-    document.querySelector('.clipboard').classList.toggle('fadeInOut')
-  })
+if (document.querySelector('#copyLink')) {
+  document.querySelector('#copyLink')
+    .addEventListener('click', (event) => {
+      event.preventDefault()
+      writeToClipboard(event.target.href)
+      document.querySelector('.clipboard').classList.toggle('fadeInOut')
+    })
+}
+
+if (document.querySelector('#title')) {
+  document.querySelector('.nav-title').innerText = document.querySelector('#title').textContent
+}
 
 function writeToClipboard (text) {
   navigator.clipboard.writeText(text)
